0cf6a9d045db2da082a9bd99faeeb2ea60759395
FredyMoraDev <a2165777@correo.uia.mx>
2023-09-06 01:47:51 -0600

    tercero

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Obtén el elemento de video
 const video = document.getElementById('miVideo');
+const playlistItems = document.querySelectorAll('.playlist li');
+
+    playlistItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const videoSrc = this.getAttribute('data-video');
+            video.src = videoSrc;
+            video.play();
+        });
+    });
 
 // Personaliza los controles del reproductor
 // Desactiva los controles nativos
